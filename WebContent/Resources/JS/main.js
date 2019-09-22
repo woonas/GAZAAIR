38793e3bf142d2af25d09b0f0cf3db5d60358b24
woonas <woonhajung@gmail.com>
2019-09-22 17:17:30 +0900

    메인페이지 수정, 네비게이션 탑버튼 추가

--- WebContent/Resources/JS/main.js
+++ WebContent/Resources/JS/main.js
@@ -1,9 +1,18 @@
 /*navi */
 //서브메뉴 마우스 오버
 $(function(){
+	//메뉴 호버시 패널생성/ 없애기
 	$(".navMenu>li").on({"mouseenter":function(){
 		$(this).children("div").css("display","block");
 	}, "mouseleave":function(){
 		$(this).children("div").css("display", "none");
-	}})
+	}});
+	
+	//탑버튼 누르면 부드럽게 스크롤하기
+	$('.topBtn>a').click(function () {
+	    $('html, body').animate({
+	      scrollTop: $($.attr(this, 'href')).offset().top
+	    }, 500);
+	    return false;
+	  });
 });
