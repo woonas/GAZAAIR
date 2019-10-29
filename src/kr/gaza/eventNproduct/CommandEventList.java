1fdf3121a817acf3f4e87b789d2d828e065ed5e9
Guyong Kim <win0202@naver.com>
2019-10-29 21:38:44 +0900

    이벤트 상세페이지, 이벤트/상품 폴더명 eventNproduct로 통일 상품,이벤트페이지에 있는 style들 다 지우고 css파일 만들어서 통합

---
+++ src/kr/gaza/eventNproduct/CommandEventList.java
@@ -0,0 +1,25 @@
+package kr.gaza.eventNproduct;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.gaza.controller.CommandService;
+
+public class CommandEventList implements CommandService {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		EventDAO dao = new EventDAO();
+		
+		List<EventVO> lst = dao.getAllRecord();
+		
+		request.setAttribute("lst", lst);
+		return "alliance_Event.jsp";
+	}
+}
