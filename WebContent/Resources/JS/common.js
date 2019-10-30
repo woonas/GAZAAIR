dd6526e870bd28e6d60c7501af665e29657bd9b1
woonas <woonhajung@gmail.com>
2019-10-30 10:23:58 +0900

    네비수정

--- WebContent/Resources/JS/common.js
+++ WebContent/Resources/JS/common.js
@@ -243,15 +243,11 @@ const strPassengers_generator = () => {
 };
 const overlay = document.querySelector('.overlay');
 
-
-//뒤로가기 기능
-const goBack = () => window.history.back();
-
 //네비게이션
 const navi = () => {
     const menus = document.querySelectorAll('#mainMenu>ul>li');
     const submenu = document.getElementById('subMenu');
-    const menubg = document.getElementById('menubg');
+    const menubg = document.getElementById('menuBg');
     menus.forEach(menu => {
        menu.addEventListener('mouseenter', () => {
            submenu.style.height = '220px';
@@ -264,19 +260,8 @@ const navi = () => {
     });
 };
 
-//상단으로 이동
-const topBtn = () => {
-    document.querySelector('.topBtn>a').addEventListener('click', () => {
-        document.documentElement.style.scrollBehavior = 'smooth';
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-        document.documentElement.style.scrollBehavior = 'auto';
-    });
-};
-
 (() => {
     navi();
-    // topBtn();
     windowClose();
     loader_generator();
     document.body.classList.add('noscroll');
