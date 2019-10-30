8aadeb2e01c79ae8f5c415470e4c2cf695bc1178
woonas <woonhajung@gmail.com>
2019-10-30 14:45:20 +0900

    로그인,로그아웃

--- src/kr/gaza/signup/CommandIdChk.java
+++ src/kr/gaza/signup/CommandIdChk.java
@@ -1,7 +1,6 @@
-package kr.gaza.eventNproduct;
+package kr.gaza.signup;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,17 +8,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import kr.gaza.controller.CommandService;
 
-public class CommandEventList implements CommandService {
+public class CommandIdChk implements CommandService {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		EventDAO dao = new EventDAO();
-		
-		List<EventVO> lst = dao.getAllRecord();
-		
-		request.setAttribute("lst", lst);
-		return "alliance_Event.jsp";
+		String userId = request.getParameter("txtID");
+		request.setCharacterEncoding("UTF-8");
+		RegisterDAO dao = new RegisterDAO();
+		boolean isOk = dao.idCheck(userId);
+		request.setAttribute("isOk", isOk);
+		return "idChk2.jsp";
 	}
+
 }
