f2041869e7921830a5c9d01c0631f9879c879507
Guyong Kim <win0202@naver.com>
2019-11-01 14:31:38 +0900

    d

--- src/kr/gaza/order/OrderVO.java
+++ src/kr/gaza/order/OrderVO.java
@@ -8,12 +8,25 @@ public class OrderVO {
     private int seatReserveNum;
     private String regDate;
 
-    private int flightNum1;
-    private int flightNum2;
-    private int flightNum3;
-    private int flightNum4;
-    private int flightNum5;
-    private int flightNum6;
+    private int[] flightNum = new int[6];
+    private int numOfFlight;
+
+    public int[] getFlightNum() {
+        return flightNum;
+    }
+
+    public void setFlightNum(int flightNum) {
+        this.flightNum[numOfFlight] = flightNum;
+        numOfFlight++;
+    }
+
+    public int getNumOfFlight() {
+        return numOfFlight;
+    }
+
+    public void setNumOfFlight(int numOfFlight) {
+        this.numOfFlight = numOfFlight;
+    }
 
     public int getOrderNum() {
         return orderNum;
@@ -53,54 +66,6 @@ public class OrderVO {
 
     public void setSeatReserveNum(int seatReserveNum) {
         this.seatReserveNum = seatReserveNum;
-    }
-
-    public int getFlightNum1() {
-        return flightNum1;
-    }
-
-    public void setFlightNum1(int flightNum1) {
-        this.flightNum1 = flightNum1;
-    }
-
-    public int getFlightNum2() {
-        return flightNum2;
-    }
-
-    public void setFlightNum2(int flightNum2) {
-        this.flightNum2 = flightNum2;
-    }
-
-    public int getFlightNum3() {
-        return flightNum3;
-    }
-
-    public void setFlightNum3(int flightNum3) {
-        this.flightNum3 = flightNum3;
-    }
-
-    public int getFlightNum4() {
-        return flightNum4;
-    }
-
-    public void setFlightNum4(int flightNum4) {
-        this.flightNum4 = flightNum4;
-    }
-
-    public int getFlightNum5() {
-        return flightNum5;
-    }
-
-    public void setFlightNum5(int flightNum5) {
-        this.flightNum5 = flightNum5;
-    }
-
-    public int getFlightNum6() {
-        return flightNum6;
-    }
-
-    public void setFlightNum6(int flightNum6) {
-        this.flightNum6 = flightNum6;
     }
 
     public String getRegDate() {
